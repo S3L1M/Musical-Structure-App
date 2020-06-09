aefaeb1337a9803c54a6bde0af07e28b778dfe6d
Mohamed Selim <dev.m.selim@gmail.com>
2020-06-09 20:23:10 +0200

    link song player activity to song list

--- app/src/main/java/com/example/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/musicalstructureapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.ReceiverCallNotAllowedException;
 import android.os.Bundle;
 import android.util.Log;
@@ -86,5 +87,7 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.SongI
     @Override
     public void onSongItemClick(Song song) {
         Toast.makeText(this, song.getTitle()+"song clicked", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, SongPlayer.class);
+        startActivity(i);
     }
 }
